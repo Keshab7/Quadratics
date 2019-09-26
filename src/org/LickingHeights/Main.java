567bbaaa2572df64970fe372c1dea0f58bc92113
Keshab <keshab.bista.2020@student.lhschools.org>
2019-09-26 09:51:13 -0400

    Added todo steps

--- src/org/LickingHeights/Main.java
+++ src/org/LickingHeights/Main.java
@@ -3,6 +3,20 @@ package org.LickingHeights;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+
+        //todo declare variables a,b,c,discriminant,axisOfSymmetry,parabolaFacing,x,y
+
+        //todo initialize manually
+
+        //todo do calculations
+
+        //todo print results
+
+        //todo not in code check results
+
+        //todo initialize scanner and redo the initialized error
+
+        //todo double check results
     }
 }
